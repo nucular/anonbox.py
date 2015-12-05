7ce7cf750b2486c88563ef332c7e94550ab3bd36
nucular <nucular@users.noreply.github.com>
2015-12-05 17:03:40 +0100

    Implemented AnonBox.check()

--- anonbox.py
+++ anonbox.py
@@ -1,6 +1,7 @@
 import os.path
 import urllib.request
 import re
+import quopri
 
 ssl = None
 try: import ssl
@@ -11,7 +12,7 @@ __dir__ = os.path.dirname(__file__)
 
 class EMail(object):
   """Parses and encapsulates a received email."""
-  def __init__(self):
+  def __init__(self, data):
     """Initializes the email instance"""
     raise NotImplementedError()
 
@@ -20,7 +21,11 @@ class AnonBox(object):
 
   def __init__(self, server="anonbox.net", https=True):
     """Creates a new inbox on the passed anonbox server."""
+    self.mails = []
+    self.valid = True
+    self.protocol = "https" if https else "http"
     self.server = server
+
     if https:
       if not ssl: raise ImportError("SSL not supported")
       context = ssl.create_default_context()
@@ -30,21 +35,21 @@ class AnonBox(object):
     else:
       self.opener = urllib.request.build_opener()
 
-    with self.opener.open("https://" + self.server + "/en") as res:
+    with self.opener.open("{}://{}/en".format(self.protocol, self.server)) as res:
       content = res.read().decode(res.info().get_content_charset() or "utf-8")
 
     m = re.search(
       r"<dd><p>([0-9a-z]{10})@([0-9a-z]{5})\."
-      + re.escape(server), content
+      + re.escape(self.server), content
     )
     if not m: raise IOError("Could not match email address in reponse")
     self.publickey = m.groups()[0]
     self.datehash = m.groups()[1]
-    self.address = "{}@{}.{}".format(self.publickey, self.datehash, server)
+    self.address = "{}@{}.{}".format(self.publickey, self.datehash, self.server)
 
     m = re.search(
       r"<dd><p><a href=\"" + re.escape(
-        ("https://" if https else "http://") + server
+        self.protocol + "://" + self.server
       ) + r"/([0-9a-z]{5})/([0-9a-z]{10})\">", content
     )
     if not m: raise IOError("Could not match access URL in reponse")
@@ -53,8 +58,22 @@ class AnonBox(object):
     self.privatekey = m.groups()[1]
 
   def check(self):
-    """Checks for new emails in the box. Returns a list of the new emails."""
-    raise NotImplementedError()
+    """Checks for new emails in the box. Returns a list of all new emails."""
+    if not self.valid:
+      return []
+
+    with self.opener.open("{}://{}/{}/{}/".format(self.protocol, self.server, self.datehash, self.publickey)) as res:
+      if res.getcode() == 404:
+        self.valid = False
+        return []
+      content = res.read().decode(res.info().get_content_charset() or "utf-8")
+
+    if not "\nFrom " in content:
+      return []
+    maildata = content.split("\nFrom ")
+    newmails = [EMail(data) for data in maildata[len(self.mails):]]
+    self.mails += newmails
+    return newmails
 
 if __name__ == "__main__":
   AnonBox()
